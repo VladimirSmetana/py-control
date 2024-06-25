26e369474caa91bb12190e37819e92958b082285
Сметана Владимир <98716102+VladimirSmetana@users.noreply.github.com>
2024-06-25 12:42:23 +0400

    Add files via upload

--- main.py
+++ main.py
@@ -9,8 +9,25 @@ Cvv = np.empty(N)
 Cvb = np.empty(N)
 Cvw = np.empty(N)
 Wind = np.empty(N)
+Ms = np.empty(N)
+W1 = np.empty(N)
+W2 = np.empty(N)
+W3 = np.empty(N)
+W4 = np.empty(N)
+W5 = np.empty(N)
+Cw1 = np.empty(N)
+Cw2 = np.empty(N)
+Cw3 = np.empty(N)
+Cw4 = np.empty(N)
+Cw5 = np.empty(N)
+Cws = np.empty(N)
 
-with open("Aero.txt", "r") as Aero:
+mass_st = 14400
+l_1 = 23720
+
+
+
+with open("Aero.txt", "r") as Aero, open("Mass.txt", "r") as Mass, open("Cw.txt", "r") as Cw:
     for i in range(N):
 
         Cwv[i] = float(Aero.readline())
@@ -20,6 +37,8 @@ with open("Aero.txt", "r") as Aero:
         Cvb[i] = float(Aero.readline())
         Cvw[i] = float(Aero.readline())
         Wind[i] = float(Aero.readline())
+        Ms[i] = float(Mass.readline())
+        Cws[i] = float(Cw.readline())
 
 h = 0.05
 uc = 0
@@ -52,11 +71,32 @@ a1 = 1.7
 a2 = 0.0004
 a3 = 10*a2
 
+
 c = 0
 
 for i in range(N):
+
+    Cw1[i] = Cws[i]
+    Cw2[i] = - Cws[i]
+    Cw3[i] = Cws[i]
+    Cw4[i] = - Cws[i]
+    Cw5[i] = Cws[i]
+
+    Cy1[i] = 0.196
+    Cy2[i] = -0.331
+    Cy3[i] = 0.464
+    Cy4[i] = -0.596
+    Cy5[i] = 0.730
+
+    W1[i] = 170.0 * mass_st/Ms[i]
+    W2[i] = 1291.6 * mass_st/Ms[i]
+    W3[i] = 4965.0 * mass_st/Ms[i]
+    W4[i] = 13564.6 * mass_st/Ms[i]
+    W5[i] = 30272.9 * mass_st/Ms[i]
+
     dv =  Cvv[i] * Wind[i] - Cvw[i] * w - Cvv[i] * v + Cvb[i] * uc
     ddw = Cwv[i] * Wind[i] - Cww[i] * w - Cwv[i] * v - Cwb[i] * uc
+
 
     dduc = (-t1*duc - uc + a0*w + a1*dw + a2*y * a3*v)/t2
     #dduc = signal(dw, Kpw, Kiw, Kdw)
